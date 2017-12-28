e2946e167233d4bb57b25305cd26cb0c7ee36526
Sherine Coronel <sherinecoronel@gmail.com>
2017-12-28 17:54:41 +0800

    Velocity chuchu

--- LatestGestures/app/src/main/java/pbl/latestgestures/Velocity.java
+++ LatestGestures/app/src/main/java/pbl/latestgestures/Velocity.java
@@ -6,14 +6,17 @@ package pbl.latestgestures;
 
 public class Velocity {
     public String velocityId;
-    public boolean singleTap, doubleTap, longPress;
+    public boolean singleTap, doubleTap, longPress, swipeX, swipeY, scroll;
 
 
-    public Velocity(String velocityId, boolean singleTap, boolean doubleTap, boolean longPress) {
+    public Velocity(String velocityId, boolean singleTap, boolean doubleTap, boolean longPress, boolean swipeX, boolean swipeY, boolean scroll) {
         this.velocityId = velocityId;
         this.singleTap = singleTap;
         this.doubleTap = doubleTap;
         this.longPress = longPress;
+        this.swipeX = swipeX;
+        this.swipeY = swipeY;
+        this.scroll = scroll;
     }
 }
 
