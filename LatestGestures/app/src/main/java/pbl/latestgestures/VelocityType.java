3a9e0f0d42c1d4415673fd36b02524737942b5ec
Sherine Coronel <sherinecoronel@gmail.com>
2017-12-31 02:24:48 +0800

    changed single tap to velocity type chuchu

--- LatestGestures/app/src/main/java/pbl/latestgestures/VelocityType.java
+++ LatestGestures/app/src/main/java/pbl/latestgestures/VelocityType.java
@@ -4,12 +4,12 @@ package pbl.latestgestures;
  * Created by Shen on 12/28/2017.
  */
 
-public class SingleTap {
+public class VelocityType {
 
     public String velocityId;
     public boolean singleTap;
 
-    public SingleTap(String velocityId, boolean singleTap) {
+    public VelocityType(String velocityId, boolean singleTap) {
         this.velocityId = velocityId;
         this.singleTap = singleTap;
     }
